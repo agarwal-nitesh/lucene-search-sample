3f10e6112de2ad785bd52638fc2fbeede05cefbc
Nitesh Agarwal <nitesh@algorythma.com>
2019-03-03 20:31:25 +0530

    Add search query and field inputs

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 ### Lucen Index Example
 
+### Build
+```$xslt
+mvn package
+java -jar target/*.jar
+```
+
 #### Run 
 ```
 mvn exec:java -D exec.mainClass=com.nitesh.newsearch.NewSearchApp
+mvn exec:java -D exec.mainClass=com.nitesh.newsearch.SearchIndexedDocument
 ```
--- src/main/java/com/nitesh/newsearch/SearchIndexedDocument.java
+++ src/main/java/com/nitesh/newsearch/SearchIndexedDocument.java
@@ -11,43 +11,61 @@ import org.apache.lucene.search.ScoreDoc;
 import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
-
-import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.Scanner;
 
 public class SearchIndexedDocument {
 
     String indexDirectoryPath;
     IndexSearcher indexSearcher;
-    QueryParser queryParser;
-    Query query;
 
     public SearchIndexedDocument(String indexDirectoryPath) throws Exception {
         this.indexDirectoryPath = indexDirectoryPath;
         Directory indexDirectory = FSDirectory.open(Paths.get(indexDirectoryPath));
         IndexReader reader = DirectoryReader.open(indexDirectory);
         this.indexSearcher = new IndexSearcher(reader);
-        this.queryParser = new QueryParser("address", new StandardAnalyzer());
     }
 
-    public TopDocs search(String searchQuery) throws Exception {
-        this.query = this.queryParser.parse(searchQuery);
-        TopDocs docs = this.indexSearcher.search(this.query, 10);
+    public TopDocs search(String searchField, String searchQuery) throws Exception {
+        QueryParser queryParser = new QueryParser(searchField, new StandardAnalyzer());
+        Query query = queryParser.parse(searchQuery);
+        TopDocs docs = this.indexSearcher.search(query, 100);
+        System.out.println("Total hits" + docs.totalHits);
         for (ScoreDoc scoreDoc : docs.scoreDocs) {
             Document doc = this.indexSearcher.doc(scoreDoc.doc);
-            System.out.println(doc.getField("address"));
+            System.out.println(doc);
         }
-        return this.indexSearcher.search(this.query, 10);
+        return docs;
     }
 
     public static void main(String[] args) {
-        try {
-            SearchIndexedDocument searchIndexedDocument = new SearchIndexedDocument("lucene-index");
-            TopDocs docs = searchIndexedDocument.search("Khirsu");
-            System.out.println(docs.totalHits);
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            try {
+                System.out.println("Enter field");
+                String field = scanner.nextLine();
+                System.out.println("Enter query string");
+                String query = scanner.nextLine();
+                SearchIndexedDocument searchIndexedDocument = new SearchIndexedDocument("lucene-index");
+                searchIndexedDocument.search(field, query);
 
-        } catch (Exception e) {
-            System.out.println("Some exception occurred while searching index: " + e.getMessage());
+            } catch (Exception e) {
+                System.out.println("Some exception occurred while searching index: " + e.getMessage());
+            }
         }
     }
+
+
+
+    /*public TopDocs search(String query, int pageNumber) throws IOException, ParseException {
+        Query searchQuery = parser.parse(query);
+        TopScoreDocCollector collector = TopScoreDocCollector.create(1000, true);
+
+        int startIndex = (pageNumber - 1) * MyApp.SEARCH_RESULT_PAGE_SIZE;
+        searcher.search(searchQuery, collector);
+
+        TopDocs topDocs = collector.topDocs(startIndex, MyApp.SEARCH_RESULT_PAGE_SIZE);
+        return topDocs;
+    }*/
 }
